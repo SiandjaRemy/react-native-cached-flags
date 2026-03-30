af1d009ecc9d8ab3e969ec4a3b43bcf2084e7006
Remy Siandja <remysiandja@gmail.com>
2026-03-30 12:40:30 -0400

    fix: attempted fix for jest test failure due to incorect mock import

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,12 +1,18 @@
 import { render } from '@testing-library/react-native';
 import { CountryFlag } from '../components/CountryFlag';
 
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
-);
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  getAllKeys: jest.fn(),
+}));
 
 jest.mock('react-native-svg', () => ({
-  SvgXml: () => null,
+  SvgUri: 'SvgUri',
+  SvgXml: 'SvgXml',
+  SvgCssUri: 'SvgCssUri',
 }));
 
 jest.mock('country-code-to-flag-emoji', () => ({
